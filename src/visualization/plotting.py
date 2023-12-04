30b8b417ed6b1108c22d7fe6123091387c765f7f
Leon Yao <leon.yao117@gmail.com>
2023-12-04 21:07:17 +0000

    improve imshow function

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -25,51 +25,34 @@ def unnormalize(image: torch.tensor) -> torch.tensor:
     return torch.clip(reverse_normalize(image), 0, 1)
 
 
-def show_cifar_images(
-    dataset: Dataset,
+def imshow_cifar(
+    images,
+    classes: List[str],
     grid_size: Tuple[int, int],
-    show_labels: Optional[bool] = True,
-    preds: Optional[List[int]] = None,
-    indices: Optional[List[int]] = None,
+    labels: Optional[List[int]],
+    preds: Optional[List[int]],
 ):
     """
     Show a grid of images from CIFAR10 dataset
     """
-    classes = [
-        "plane",
-        "car",
-        "bird",
-        "cat",
-        "deer",
-        "dog",
-        "frog",
-        "horse",
-        "ship",
-        "truck",
-    ]
-    if preds is not None:
-        assert len(preds) == grid_size[0] * grid_size[1]
-    if dataset is None:
-        raise ValueError("dataset cannot be None")
+
     n1, n2 = grid_size
     plt.subplots(n1, n2, figsize=(n2 * 1.5, n1 * 1.5))
     for i in range(n2):
         for j in range(n1):
-            count = i * grid_size[0] + j
-            if indices is not None:
-                idx = indices[count]
-            else:
-                idx = count
-            img, label = dataset[idx]
-            label = int(label)
+            idx = i * grid_size[0] + j
+
             plt.subplot(n1, n2, idx + 1)
-            imshow_image(img)
-            if show_labels:
-                plt.title(classes[label])
+            imshow_image(images[idx])
+            title = ""
+            color = "black"
+            if labels is not None:
+                title += classes[labels[idx]]
             if preds is not None:
-                if label != preds[idx]:
-                    color = "red"
-                plt.title(f"{classes[label]}-{classes[preds[idx]]}", color=color)
+                title += classes[preds[idx]]
+            if preds is not None and labels is not None:
+                color = "red" if labels[idx] == preds[idx] else "green"
+            plt.title(title, color=color)
     plt.tight_layout()
     plt.show()
 
